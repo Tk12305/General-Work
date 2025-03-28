9904c47877fd947a5fd99ad6a3d29af2ebe4bb29
Toby king <tbgglking@gmail.com>
2025-03-28 22:59:39 +0000

    uploading more code

---
+++ Alarm.py
@@ -0,0 +1,7 @@
+alarm = input ("is the alarm set Y/N?")
+motion = input ("has motion been detected Y/N?")
+
+if alarm == "y" or motion == "y" :
+    print ("HELP MY ALARM IS GOING !! CALL THE POLICE")
+else:
+    print ("Relax no danger")

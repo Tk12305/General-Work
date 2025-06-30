42e52dc03f6e3bd1534eb6ce7e6df4d64ec4b76c
Toby king <tbgglking@gmail.com>
2025-06-30 22:57:53 +0000

    more

---
+++ Random num generator.py
@@ -0,0 +1,18 @@
+import random
+# This script generates a random integer between 1 and 1000
+print(random.randint(1, 1000))
+print(random.randint(1, 2000))
+print(random.randint(1, 4000))
+print(random.randint(1, 8000))
+print(random.randint(1, 16000))
+print(random.randint(1, 32000))
+print(random.randint(1, 64000))
+print(random.randint(1, 128000))
+print(random.randint(1, 256000))
+print(random.randint(1, 512000))
+print(random.randint(1, 1024000))
+print(random.randint(1, 2048000))
+print(random.randint(1, 4096000))
+print(random.randint(1, 8192000))
+print(random.randint(1, 16384000))
+print(random.randint(1, 32768000))

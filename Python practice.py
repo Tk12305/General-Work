2bfa065e05d499f8d041526dd99fb41d9de3a1cf
Toby king <tbgglking@gmail.com>
2025-06-30 22:54:07 +0000

    Python practice

---
+++ Python practice.py
@@ -0,0 +1,6 @@
+a = 7
+
+if a == 60:
+    print("a is 60")
+elif a == 7:
+    print("a is 7")
---
+++ Triangle python test.py
@@ -0,0 +1,8 @@
+a = 30
+b = 60
+c = 90
+
+if a + b + c == 180:
+    print ("Angles form a triangle")
+else:
+    print ("Angles do not form a triangle")

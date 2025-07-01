64cf70b878125016cc57273c70b9b385678f369e
Toby king <tbgglking@gmail.com>
2025-07-01 23:38:48 +0000

    done

---
+++ More development.py
@@ -0,0 +1,24 @@
+a = 5
+b = 10
+c = 20
+d = 40
+e = 80
+f = 160
+g = 320
+h = 640
+i = 1280
+j = 2560
+
+while True:
+    if a + b < c:
+        print("a + b is greater than c")
+    if d - e < f:
+        print("d - e is less than f")
+    if g * h > i:
+        print("g * h is greater than i")
+    if j / d == h:
+        print("j divided by d equals h")
+    else:
+        print("One or more conditions not met")
+
+    break

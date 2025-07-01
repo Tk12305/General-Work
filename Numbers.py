252f18929bb817d2ebf294032cb5c286d8b07081
Toby king <tbgglking@gmail.com>
2025-07-01 23:28:12 +0000

    Done more python

---
+++ Numbers.py
@@ -0,0 +1,26 @@
+a = 5
+b = 10
+c = 20
+d = 40
+e = 80
+f = 160
+g = 320
+h = 640
+i = 1280
+j = 2560
+k = 5120
+l = 10240
+m = 20480
+
+if a * b * f > c:
+    print("a * b * f is more than c")
+if d + e < k:
+    print("d + e is less than k")
+if l - h == i:
+    print("l - h is equal to i")
+if j / g == 8:
+    print("j / g is equal to 8")
+if a + b + c + d + e + f + g + h + i + j + k + l + m == 40955:
+    print("The sum of all variables is 40955")
+else:
+    print("The sum of all variables is not 40955")

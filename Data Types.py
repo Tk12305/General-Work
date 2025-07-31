2c844a3e84e6b67412a44e5ac6f25aa6f20249bf
Toby king <tbgglking@gmail.com>
2025-07-31 23:05:42 +0000

    Data Types Complete

---
+++ Data Types.py
@@ -0,0 +1,45 @@
+a = "Hello World"
+print(a)
+print(type(a))
+b = 20
+print(b)
+print(type(b))
+c = 20.5
+print(c)
+print(type(c))
+d = 1j
+print(d)
+print(type(d))
+e = ["acdc", "blur", "megadeath"]
+print(e)
+print(type(e))
+f = ("metallica", "linkin park", "the beatles")
+print(f)
+print(type(f))
+g = range(6)
+print(g)
+print(type(g))
+h = {"name": "elliot", "age" : 15}
+print(h)
+print(type(h))
+i = {"foo fighters", "abba", "oasis"}
+print(i)
+print(type(i))
+j = frozenset({"nirvana", "queen", "the rolling stones"})
+print(j)
+print(type(j))
+k = True
+print(k)
+print(type(k))
+l = b"Hello"
+print(l)
+print(type(l))
+m = bytearray(5)
+print(m)
+print(type(m))
+n = memoryview(bytes(5))
+print(n)
+print(type(n))
+o = None
+print(o)
+print(type(o))

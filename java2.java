a5afed5eea1bcbe914208ac4d64b7e987b9649db
Toby king <tbgglking@gmail.com>
2025-08-28 21:56:09 +0000

    Started to learn java via w3schools

---
+++ java2.java
@@ -0,0 +1,7 @@
+public class java2 {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+    System.out.println("I am Learning Java");
+    System.out.println("It is awesome!");
+    }
+}
---
+++ java3.java
@@ -0,0 +1,6 @@
+public class java3 {
+  public static void main(String[] args) {
+    System.out.println("This sentence will work!");
+    System.out.println("This sentence will produce an error if quotation marks are not placed or opened and closed properly");
+    }
+}

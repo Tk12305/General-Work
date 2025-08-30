d5f2d0233eba11dd65f8c55cd09fa86a028c8a5e
Toby king <tbgglking@gmail.com>
2025-08-30 17:38:54 +0000

    Java: more sections on w3schools complete

---
+++ JavaIdentifiers.java
@@ -0,0 +1,8 @@
+public class JavaIdentifiers {
+    public static void main(String[] args) {
+        int minutesPerHour = 60;
+        int m =60;
+        System.out.println(minutesPerHour);
+        System.out.println(m);
+    }
+}
---
+++ JavaVariablesReallifeExamples.java
@@ -0,0 +1,17 @@
+public class JavaVariablesReallifeExamples {
+    public static void main(String[] args) {
+        // Student data
+        String studentName = "Elliot Rhymes"; // Name of the student
+        int studentID = 4321; // Unique ID for the student
+        int studentAge = 16; // Age of the student
+        float studentFee = 75.35f;
+        char studentGrade = 'B'; // Grade of the student
+
+        // Print Variables
+        System.out.println("Student Name: " + studentName);
+        System.out.println("Student ID: " + studentID);
+        System.out.println("Student age: " + studentAge);
+        System.out.println("Student Fee: " + studentFee);
+        System.out.println("Student grade: " + studentGrade);
+    }
+}

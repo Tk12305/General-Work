eaa88037f13f04412d2b28d01ea8b95326535643
Toby king <tbgglking@gmail.com>
2025-09-01 20:24:52 +0000

    More java

---
+++ JavaVarDataType.java
@@ -0,0 +1,6 @@
+public class JavaVarDataType {
+    public static void main(String[] args) {
+        var x = 5; // x is an integer
+        System.out.println(x);
+    }
+}
---
+++ JavaVarDataTypeExample.java
@@ -0,0 +1,15 @@
+public class JavaVarDataTypeExample {
+    public static void main(String[] args) {
+        var myNum = 5;               // Integer
+        var myDouble = 9.98;         // Double
+        var myChar = 'D';            // Character
+        var myBoolean = true;        // Boolean
+        var mystring = "Hello";      // String
+
+        System.out.println(myNum);
+        System.out.println(myDouble);
+        System.out.println(myChar);
+        System.out.println(myBoolean);
+        System.out.println(mystring);
+    }
+}

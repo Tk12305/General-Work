29b55ee57bfac1399ef474f347e97b500177bf24
Toby king <tbgglking@gmail.com>
2025-09-02 23:06:08 +0000

    More Java from w3schools

---
+++ JavaConcatenationinSentences.java
@@ -0,0 +1,7 @@
+public class JavaConcatenationinSentences {
+    public static void main(String[] args) {
+        String name = "John";
+        int age = 18;
+        System.out.println("My name is " + name + " and I am " + age + " years old. ");
+    }
+}
---
+++ JavaTheConcatMethod.java
@@ -0,0 +1,7 @@
+public class JavaTheConcatMethod {
+    public static void main(String[] args0) {
+        String firstName = "John ";
+        String lastName = "Doe";
+        System.out.println(firstName.concat(lastName));
+    }
+}

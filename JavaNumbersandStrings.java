436641857ce545ddf2630df2edea2295154b6f05
Toby king <tbgglking@gmail.com>
2025-09-09 07:57:57 +0000

    Java - numbers and strings

---
+++ JavaNumbersandStrings.java
@@ -0,0 +1,8 @@
+public class JavaNumbersandStrings {
+    public static void main(String[] args) {
+        int x = 10;
+        int y = 20;
+        int z = x + y;
+        System.out.println("The sum of x and y is: " + z);
+    }
+}

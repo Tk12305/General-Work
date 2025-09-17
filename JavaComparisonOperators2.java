088f564c17e4c319e5d2a071acb9cc4e5e0ddbc3
Toby king <tbgglking@gmail.com>
2025-09-17 20:17:46 +0000

    java comparison operators

---
+++ JavaComparisonOperators2.java
@@ -0,0 +1,8 @@
+public class JavaComparisonOperators2 {
+    public static void main(String[] args) {
+        int age = 18;
+        
+        System.out.println(age >= 18); // true, old enought to vote
+        System.out.println(age < 18);  // false, not old enough
+    }
+}
---
+++ JavaComparisonOperators3.java
@@ -0,0 +1,8 @@
+public class JavaComparisonOperators3 {
+    public static void main(String[] args) {
+        int passwordLength = 5;
+
+        System.out.println(passwordLength >= 8); // false, too short
+        System.out.println(passwordLength < 8);  // true, needs more characters
+    }
+}

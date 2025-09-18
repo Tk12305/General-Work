0cf4771c2b73a0bb271004a91a83eab65e44d924
Toby king <tbgglking@gmail.com>
2025-09-18 19:58:36 +0000

    Special characters (Java)

---
+++ JavaSpecialCharacters2.java
@@ -0,0 +1,6 @@
+public class JavaSpecialCharacters2 {
+    public static void main(String[] args) {
+        String txt = "It\'s alright.";
+        System.out.println(txt);
+    }
+}
---
+++ JavaSpecialCharacters3.java
@@ -0,0 +1,6 @@
+public class JavaSpecialCharacters3 {
+    public static void main(String[] args) {
+        String txt = "The character \\ is called backlash.";
+        System.out.println(txt);
+    }
+}

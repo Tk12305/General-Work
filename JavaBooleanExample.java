1f120a1d346b33f91506b62b76b1feba98b4a686
Toby king <tbgglking@gmail.com>
2025-10-20 23:00:04 +0000

    Java - Boolean

---
+++ JavaBooleanExample.java
@@ -0,0 +1,7 @@
+public class JavaBooleanExample {
+    public static void main(String[] args) {
+        int myAge = 25;
+        int votingAge = 18;
+        System.out.println(myAge >= votingAge);
+    }
+}
---
+++ JavaBooleanExperessionsEasierWay.java
@@ -0,0 +1,5 @@
+public class JavaBooleanExperessionsEasierWay {
+    public static void main(String args[]) {
+        System.out.println(10 > 9); // returns true, because 10 is greater than 9
+    }
+}

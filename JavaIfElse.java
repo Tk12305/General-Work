b442436ea8866594ebe5989f54c435b9311f82ba
Toby king <tbgglking@gmail.com>
2025-11-07 09:13:59 +0000

    Javas - IfElse

---
+++ JavaIfElse.java
@@ -0,0 +1,16 @@
+public class JavaIfElse {
+    public static void main(String[] args) {
+        int Number1 = 5;
+        int Number2 = 15;
+
+        if (Number1 >=4) {
+            System.out.println("Number1 is greater than or equal to 4");
+        } else if (Number2 <= 10) {
+            System.out.println("Number2 is smaller or equal to 10");
+        } else {
+            if (Number1 + Number2 > 20) {
+                System.out.println("The sum of Number1 and Number2 is greater than 20");
+        }
+        }
+    }
+}

11959aaad3c360825cd715c461417a619bac3324
Toby king <tbgglking@gmail.com>
2025-12-09 16:58:07 +0000

    fixing stuff

--- Java/JavaVariables6.java
+++ Java/JavaVariables6.java
@@ -1,7 +1,7 @@
 public class JavaVariables6 {
     public static void main(String[] args){
-        final int myNumber = 15;
-        myNumber = 20; // will generate an error: cannot assign a value to a final variable
-        System.out.println(myNumber);
+        final int num = 15;
+        num = 20; // will generate an error: cannot assign a value to a final variable
+        System.out.println(num);
     }
 }

7b0c619ec6c68c67e3e206adfdbbbaec7b36f6c1
Toby king <tbgglking@gmail.com>
2025-12-09 18:15:35 +0000

    Doing some python practice

---
+++ x&y.py
@@ -0,0 +1,10 @@
+x = 56
+y = 15
+
+if x + y == 91:
+    print("The sum is 91")
+else:
+    print("The sum is more or less than 91")
+    print("y =", y)
+    print("x =", x)
+    print("x + y =", x + y)
